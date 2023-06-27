15814bd023374fd6739161a5aa92f3820b5d54f7
Shogo Kitada <contact@shogo0x2e.com>
2023-06-27 14:19:32 +0900

    ページタイトルを変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from "react";
-import Image from "next/image";
 
 import ResponsiveAppBar from "@/components/ResponsiveAppBar";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -87,7 +91,7 @@ export default function Home() {
             }}
           />
         </Box>
-        
+
       </Box>
 
     </ThemeProvider>
